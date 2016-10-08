72104b302a3661ad9239efd35e1286f8836af372
sirobuw <sirobuw@gmail.com>
2016-10-08 17:32:55 +0800

    store local datetime

--- calparser.py
+++ calparser.py
@@ -1,13 +1,36 @@
 # encoding: utf-8
 
-file_in = open('target.ics', mode='r', encoding='utf8')
+from datetime import datetime, timezone
+
+file_in = open('targetcal.ics', mode='r', encoding='utf8')
 file_out = open('parsed.txt', mode='w', encoding='utf8')
+
+caldict = {}
+datekey = ''
 
 for line in file_in:
     if 'DTSTART' in line:
-        file_out.write(line[8:])
+        utctime = datetime.strptime(line[8:].strip(), '%Y%m%dT%H%M%SZ')
+        localtime = utctime.replace(tzinfo=timezone.utc).astimezone(None)
+        datekey = localtime.strftime('%Y%m%d')
+
+        if datekey in caldict:
+            clockinoutlist = caldict.get(datekey)
+            clockinoutlist.append(localtime)
+
+        else:
+            clockinoutlist = [localtime]
+
+        caldict[datekey] = clockinoutlist
+
     elif 'SUMMARY' in line:
-        file_out.write(line[8:])
+        clockinoutlist = caldict.get(datekey)
+        clockinoutlist.append(line[8:].strip())
+        caldict[datekey] = clockinoutlist
+
+for key, value in sorted(caldict.items()):
+    line = key + ' ' + ''.join(value) + '\n'
+    file_out.write(line)
 
 file_in.close()
 file_out.close()
