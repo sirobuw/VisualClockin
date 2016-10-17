6a712f184e33cf7d3c6a7a5b82dd0dbebe9d10d1
sirobuw <sirobuw@gmail.com>
2016-10-18 03:20:11 +0800

    change parsed format for plotdb

--- calparser.py
+++ calparser.py
@@ -3,7 +3,10 @@
 from datetime import datetime, timezone, timedelta
 
 file_in = open('targetcal.ics', mode='r', encoding='utf8')
-file_out = open('parsed.txt', mode='w', encoding='utf8')
+file_out = open('parseddataset.csv', mode='w', encoding='utf8')
+
+columnname = '"日期","上班","下班"' + '\n'
+file_out.write(columnname)
 
 caldict = {}
 datekey = ''
@@ -29,17 +32,27 @@ for line in file_in:
         caldict[datekey] = clockinoutlist
 
 for key, value in sorted(caldict.items()):
+    if value[1] == '加班':
+        continue
+
     if value[1] == '下班':
         changeorder = [2, 3, 0, 1]
         value = [value[i] for i in changeorder]
 
     if value[2] < value[0]:
         value[2] += timedelta(days=1)
+        value[2] = value[2].strftime('%H%M')
+        value[2] = int(value[2]) + 2400
+    else:
+        value[2] = value[2].strftime('%H%M')
 
-    value[0] = value[0].strftime('%Y/%m/%d %H:%M')
-    value[2] = value[2].strftime('%Y/%m/%d %H:%M')
+    # value[0] = value[0].strftime('%Y/%m/%d %H:%M')
+    # value[2] = value[2].strftime('%Y/%m/%d %H:%M')
+    value[0] = value[0].strftime('%H%M')
+    # value[2] = value[2].strftime('%H%M')
 
-    line = key + ' ' + ', '.join(value) + '\n'
+    # line = key + ' ' + ', '.join(value) + '\n'
+    line = '"' + key + '","' + value[0] + '","' + str(value[2]) + '"\n'
     file_out.write(line)
 
 file_in.close()
